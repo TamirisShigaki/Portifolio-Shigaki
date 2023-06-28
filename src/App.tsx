83d1c30bda944b1f52c06e756ce98ba5ae5be30b
TamirisShigaki <tamiris.endo@altaplus.com.br>
2023-06-27 21:56:18 -0300

    build: :heavy_plus_sign: Adicionando e configurando tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 function App() {
 
-  return (
-    <>
-      <h1>
-        Portifolio Tamiris
-      </h1>
-    </>
-  )
+    return (
+		<>
+			<h1 className="bg-green-300">
+				Portifolio Tamiris
+			</h1>
+		</>
+    )
 }
 
 export default App
